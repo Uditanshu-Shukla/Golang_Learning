ec5ac12e4d7adf7dcf8cbd00ca18ff92bd6c4f0e
Uditanshu <uditanshushukla34@gmail.com>
2022-07-05 18:26:44 +0530

    commiting day 1

---
+++ variables/main.go
@@ -0,0 +1,39 @@
+package main
+ 
+import "fmt"
+
+// func main(){
+// 	var num,age int=12,24
+// 	fmt.Println(num,age)
+// }
+
+// func main(){
+// 	var(
+// 		name="uditanshu"
+// 		age=23
+// 		height int 
+// 		weight int
+// 	)
+// 	fmt.Println(name,age,height,weight)
+// }
+
+// func main(){
+// 	count:=12
+// 	name,age:="udit",23
+// 	fmt.Println(count,name,age)
+
+// }
+
+func main(){
+	var a bool
+	fmt.Println(a)
+	a=true
+	fmt.Println(a)
+	b:=12
+	fmt.Println(b)
+	c,d:=2.37,4.57
+	diff:=c-d
+	fmt.Println(c+d,diff)
+	f:=complex(2,7)
+	fmt.Println(f)
+}
